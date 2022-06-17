4fdff9bb1e10bdce08d8a8a18944735051dca91d
Александр Сапрыкин <alamana13@mail.ru>
2022-06-17 14:00:20 +0300

    Сделал рабочую версию которая запросто вычисляет выражения. В идеале добавить инфу о том как пользоваться им

--- calc_bot.py
+++ calc_bot.py
@@ -1,11 +1,12 @@
 import config
 import telebot
+import sympy
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):# Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, f'Result =  {sympy.sympify(message.text)}')
 
 if __name__ == '__main__':
     bot.infinity_polling()
