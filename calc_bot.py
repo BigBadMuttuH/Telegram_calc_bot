4ab92d7d7f1ac1c431061c0075974aa9900b7ca8
Александр Сапрыкин <alamana13@mail.ru>
2022-06-17 14:34:31 +0300

    Накидал дополнительно todo как можно дополнить код. Вечером соберемся и распределим между собой это всё

--- calc_bot.py
+++ calc_bot.py
@@ -5,8 +5,13 @@ import sympy
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):# Название функции не играет никакой роли
+def calculating_message(message):
     bot.send_message(message.chat.id, f'Result =  {sympy.sympify(message.text)}')
 
 if __name__ == '__main__':
     bot.infinity_polling()
+
+#TODO сделать функцию выводящую info по требованию
+#TODO сделать функцию выводящую инфу о создателях кто и что сделал about
+#TODO добавить вычисление еще чего-нибудь синусов или расстояний между точками или объемов и т. п.
+#
