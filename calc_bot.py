5548aed4020fdf1ed0e29d8ac0bac7393869a23c
Александр Сапрыкин <alamana13@mail.ru>
2022-06-18 14:38:36 +0300

    Добавил функцию с выведением всех атрибутов и сделал приветствие.

--- calc_bot.py
+++ calc_bot.py
@@ -1,17 +1,24 @@
+import markup as markup
+
 import config
 import telebot
+from telebot import types
 import sympy
 
 bot = telebot.TeleBot(config.token)
 
+
 @bot.message_handler(content_types=["text"])
 def calculating_message(message):
-    bot.send_message(message.chat.id, f'Result =  {sympy.sympify(message.text)}')
+    if message not in ['start', 'help', '/start', '/help']:
+        bot.send_message(message.chat.id, f'Result =  {sympy.sympify(message.text)}')
+
+@bot.message_handler(commands=['text'])
+def help_message(message):
+    bot.send_message(message.chat.id, message.text('Привет'))
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
 
 #TODO сделать функцию выводящую info по требованию
-#TODO сделать функцию выводящую инфу о создателях кто и что сделал about
-#TODO добавить вычисление еще чего-нибудь синусов или расстояний между точками или объемов и т. п.
-#
